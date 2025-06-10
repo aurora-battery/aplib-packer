b44d2ddec966a23c00d1d2c21cc4f37a69e00b6c
dbeinder <david.beinder@chronos.li>
2025-06-10 14:22:10 +0200

    LiFW header, custom CRC, 32k max offset

--- appack/Program.cs
+++ appack/Program.cs
@@ -8,6 +8,13 @@ namespace appack
 {
     class Program
     {
+        const byte FWFILE_BOXMON_V1 = 0x01;
+        const byte FWFILE_TOPSTM_V1 = 0x02;
+        const byte FWFILE_TOPESP_V1 = 0x03; // not used
+        const byte FWFILE_COMPR_NONE = 0x00;
+        const byte FWFILE_COMPR_APLIB = 0x01;
+        const byte FWFILE_COMPR_UPKR = 0x02;
+
         static void Main(string[] args)
         {
             if (!ValidInput(args))
@@ -31,19 +38,31 @@ namespace appack
             {
                 byte[] compressed = input;
                 bool goodHeader = true;
-                bool hasHeader = input[0] == 'A' && input[1] == 'P' && input[2] == '3' && input[3] == '2';
+                bool hasHeader = input[0] == 'L' && input[1] == 'i' && input[2] == 'F' && input[3] == 'W';
                 if (hasHeader)
                 {
                     compressed = input[24..];
-                    goodHeader &= BitConverter.ToUInt32(input[4..8]) == 24;
+                    string compression = input[4] switch
+                    {
+                        FWFILE_COMPR_NONE => "none",
+                        FWFILE_COMPR_APLIB => "aplib",
+                        FWFILE_COMPR_UPKR => "upkr"
+                    };
+                    string fwType = input[5] switch
+                    {
+                        FWFILE_BOXMON_V1 => "BoxMon",
+                        FWFILE_TOPSTM_V1 => "TopBox"
+                    };
+                    Console.WriteLine($"Type: {fwType} Compression: {compression}");
+                    if (input[4] != FWFILE_COMPR_APLIB) Environment.Exit(-1);
                     goodHeader &= BitConverter.ToUInt32(input[8..12]) == (input.Length - 24);
-                    goodHeader &= BitConverter.ToUInt32(input[12..16]) == Crc32.HashToUInt32(compressed);
+                    goodHeader &= BitConverter.ToUInt32(input[12..16]) == LiFwCrc.HashToUInt32(compressed);
                 }
                 output = aplib.decode(compressed);
                 if (hasHeader)
                 {
                     goodHeader &= BitConverter.ToUInt32(input[16..20]) == output.Length;
-                    goodHeader &= BitConverter.ToUInt32(input[20..24]) == Crc32.HashToUInt32(output);
+                    goodHeader &= BitConverter.ToUInt32(input[20..24]) == LiFwCrc.HashToUInt32(output);
                     if (!goodHeader) Console.WriteLine("INVALID HEADER!");
                 }
             }
@@ -56,13 +75,16 @@ namespace appack
 
         private static byte[] AddHeader(byte[] input, byte[] compressed)
         {
+            bool isWb0Fw = input[20..24].SequenceEqual(Encoding.ASCII.GetBytes("EULB"));
+            byte fwType = isWb0Fw ? FWFILE_BOXMON_V1 : FWFILE_TOPSTM_V1;
+
             var buf = new MemoryStream();
-            buf.Write(Encoding.ASCII.GetBytes("AP32"));
-            buf.Write(BitConverter.GetBytes((UInt32)0x18));
+            buf.Write(Encoding.ASCII.GetBytes("LiFW"));
+            buf.Write(new byte[] { FWFILE_COMPR_APLIB, fwType, 0x00, 0x00 });
             buf.Write(BitConverter.GetBytes((UInt32)compressed.Length));
-            buf.Write(BitConverter.GetBytes(Crc32.HashToUInt32(compressed)));
+            buf.Write(BitConverter.GetBytes(LiFwCrc.HashToUInt32(compressed)));
             buf.Write(BitConverter.GetBytes((UInt32)input.Length));
-            buf.Write(BitConverter.GetBytes(Crc32.HashToUInt32(input)));
+            buf.Write(BitConverter.GetBytes(LiFwCrc.HashToUInt32(input)));
             buf.Write(compressed);
             return buf.ToArray();
         }
@@ -129,10 +151,10 @@ namespace appack
 
         private static void PrintUsage()
         {
-            System.Console.WriteLine("appack 1.3");
+            System.Console.WriteLine($"appack 1.3 (LiFW header, max offset: {constant.threshold_offset})");
             System.Console.WriteLine();
             System.Console.WriteLine("Usage:");
-            System.Console.WriteLine("appack d|e infile outfile");
+            System.Console.WriteLine("appack d[ecode]|e[ncode] infile outfile");
         }
         private static bool ValidInput(string[] input)
         {
@@ -145,5 +167,27 @@ namespace appack
             );
         }
 
+    }
+
+    static class LiFwCrc
+    {
+        static UInt32[] short_poly_lut = { 0x00000000, 0x3F079B52, 0x7E0F36A4, 0x4108ADF6, 0xFC1E6D48, 0xC319F61A, 0x82115BEC, 0xBD16C0BE, 0xA814498F, 0x9713D2DD, 0xD61B7F2B, 0xE91CE479, 0x540A24C7, 0x6B0DBF95, 0x2A051263, 0x15028931 };
+        static UInt32[] long_poly_lut =  { 0x00000000, 0x934E3AE6, 0x89BC3E5F, 0x1AF204B9, 0xBC58372D, 0x2F160DCB, 0x35E40972, 0xA6AA3394, 0xD79025C9, 0x44DE1F2F, 0x5E2C1B96, 0xCD622170, 0x6BC812E4, 0xF8862802, 0xE2742CBB, 0x713A165D };
+
+
+        static UInt32 CRC32_Calculate_Ex(ReadOnlySpan<byte> data, UInt32 prev_crc32, bool long_poly)
+        {
+            UInt32[] lut = long_poly ? long_poly_lut : short_poly_lut;
+            UInt32 crc = ~prev_crc32;
+            foreach (var current in data)
+            {
+                crc = lut[(crc ^ current) & 0x0F] ^ (crc >> 4);
+                crc = lut[(crc ^ (current >> 4)) & 0x0F] ^ (crc >> 4);
+            }
+
+            return ~crc;
+        }
+
+        public static UInt32 HashToUInt32(ReadOnlySpan<byte> data) => CRC32_Calculate_Ex(data, 0, true);
     }
 }
